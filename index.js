5f58a9cc440d6a371f702ada6951036eea3a1cb6
Wen Xiao <wennstyle@gmail.com>
2021-11-14 16:50:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,95 @@
 /* Your Code Here */
+function createEmployeeRecord(recordArray){
+
+    return {
+        firstName: recordArray[0],
+        familyName: recordArray[1],
+        title: recordArray[2],
+        payPerHour: recordArray[3],
+    
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+
+function createEmployeeRecords(nestedArray){
+    //using map method to return as many arrays of objects
+    return nestedArray.map(arrays => createEmployeeRecord(arrays))
+    
+}
+
+
+function createTimeInEvent(dateTimeInStamp){
+    //Create an object and push this oject into timeInEvents
+    let obj = {
+        type: "TimeIn",
+        date: dateTimeInStamp.slice(0,10),
+        hour: parseInt(dateTimeInStamp.slice(11,15),10)
+    }
+
+    this.timeInEvents.push(obj)
+    return this
+}
+
+
+function createTimeOutEvent(dateTimeOutStamp){
+    //Create an object and push this object into timeOutEvents
+        let obj = {
+            type: "TimeOut",
+            date: dateTimeOutStamp.slice(0,10),
+            hour: parseInt(dateTimeOutStamp.slice(11,15),10)
+        }
+    
+        this.timeOutEvents.push(obj)
+        return this
+}
+
+
+function hoursWorkedOnDate(date){
+    let timeIn = this.timeInEvents.filter(function(ele){
+        return ele.date === date;
+    })
+    
+    let timeOut = this.timeOutEvents.filter(function(ele){
+        return ele.date === date;
+    })
+
+    let hoursWorked = timeOut[0].hour - timeIn[0].hour
+    //console.log(hoursWorked)
+    return hoursWorked/100
+}
+
+
+function wagesEarnedOnDate(date){
+
+return hoursWorkedOnDate.call(this, date) * this.payPerHour
+//cannot use hoursWordedOnDate(date) because there's no object in hoursWorkedOnDate for 'this'.
+//this is defined b/c of cRecord (from test scope). you need the .call to pass the object into the hoursWorkedOnDate function
+}
+
+
+function findEmployeeByFirstName(employeeRecords, firstName){
+//console.log(employeeRecords)
+let nameMatch = employeeRecords.filter(function(ele){
+    return ele.firstName === firstName
+})
+
+console.log(nameMatch[0].familyName)
+console.log(nameMatch[0].firstName)
+return nameMatch[0].familyName
+}
+
+
+function calculatePayroll(employeeRecords){
+    let total = 0
+    for(let i = 0; i< employeeRecords.length; i++){
+        
+        total += allWagesFor.call(employeeRecords[i])
+        //console.log(total)
+    }
+    return total
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
